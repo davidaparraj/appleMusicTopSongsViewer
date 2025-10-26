6aa518544236f281c246a7b8cc48d32508ac2178
David Parra <davida.parra10@gmail.com>
2025-10-25 23:20:39 -0400

    Optimized data organization

--- itunes.py
+++ itunes.py
@@ -1,13 +1,14 @@
+import json
 import requests
 
 # Function to raise exceptions for artist's name and number of songs
 def checkArtist(name):
     if not isinstance(name, str):
-        raise TypeError("Enter a valid name") 
+        raise TypeError("Enter a valid name") # Raise exception if data not inserted correctly
 
 def checkSongs(songs):
     if not isinstance(songs, int):
-        raise TypeError("Enter a valid number") # Raise exception if data not inserted correctly
+        raise TypeError("Enter a valid number")
     
 # Loop to check for correct input
 while True:
@@ -24,10 +25,10 @@ while True:
 url = "https://itunes.apple.com/search"
 params = {"entity": "song", "limit": songs, "term": artist}
 response = requests.get(url, params=params)
-
+    
 # The data we requested gets back as JSON
 o = response.json()
-# Organize data to show top songs of artist
+# Organize data to show top songs of artist and other details
 print(f"{artist} top {songs} results:\n")
-for result in o["results"]:
-    print(result["trackName"])
+for i, result in enumerate(o["results"], start=1):
+    print(f"{i:>2}. {result["trackName"]} - {result["collectionName"]} ({result["releaseDate"][:4]})") # Slice release date to show only year
