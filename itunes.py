5447f76c1a0a1fcca9f982859ab70e15391c9253
David Parra <davida.parra10@gmail.com>
2025-10-25 22:59:56 -0400

    Modified file for optimization
    Added input validation for artist name and number of songs.

--- itunes.py
+++ itunes.py
@@ -1,18 +1,33 @@
-import json
 import requests
-import sys
 
-#if len(sys.argv) != 2:
-    #sys.exit()
-artist = "Kendrick Lamar"
+# Function to raise exceptions for artist's name and number of songs
+def checkArtist(name):
+    if not isinstance(name, str):
+        raise TypeError("Enter a valid name") 
 
-#Request the information of one artist from itunes
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + artist)
+def checkSongs(songs):
+    if not isinstance(songs, int):
+        raise TypeError("Enter a valid number") # Raise exception if data not inserted correctly
+    
+# Loop to check for correct input
+while True:
+    try:
+        artist = input("Enter the name of the desired artist: ")
+        checkArtist(artist)
+        songs = int(input("Enter the amount of songs to display: "))
+        checkSongs(songs)
+    except TypeError as e:
+        print(f"Invalid input: {e}")
+    break
 
-# print(response.json()) The data we requested gets backs as json
-#print(json.dumps(response.json(), indent=2)) #the data is indented and is better organized
+#Request the information of one artist from itunes API
+url = "https://itunes.apple.com/search"
+params = {"entity": "song", "limit": songs, "term": artist}
+response = requests.get(url, params=params)
 
-
+# The data we requested gets back as JSON
 o = response.json()
+# Organize data to show top songs of artist
+print(f"{artist} top {songs} results:\n")
 for result in o["results"]:
     print(result["trackName"])
